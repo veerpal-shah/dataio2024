b6e5edad82fb04dcf4d0c16db8522b907bc54a13
Donovin Natividad <donovincall@gmail.com>
2024-02-24 15:37:23 -0500

    Added tripduration pictures

--- python/bikelanes.py
+++ python/bikelanes.py
@@ -23,4 +23,30 @@ bike_lanes = bike_lanes.to_crs("EPSG:4326")
 
 folium.GeoJson(bike_lanes).add_to(chicago_map)
 
+# Adding two markers for Most used station and least used station
+
+most_used_arr = [[[lat, long], "Station #", "# rented"]]
+
+
+for tup in most_used_arr:
+    str = "Station {}, {} bikes rented".format(tup[2], tup[3])
+    folium.Marker(location=tup[0], popup=str, icon=folium.Icon(color="blue")).add_to(chicago_map)
+    
+least_used_arr = [[[lat, long], "Station #", "# rented"]]
+
+for tup in least_used_arr:
+    str = "Station {}, {} bikes rented".format(tup[2], tup[3])
+    folium.Marker(location=tup[0], popup=str, icon=folium.Icon(color="red")).add_to(chicago_map)
+
+
+most_used_location = [41.89228, -87.61204]
+least_used_location = [41.79, -87.65]
+
+most_used_label = "Station 13022, 215,724,292,269 bikes rented"
+least_used_label = "Station 769, 214,371 bikes rented"
+
+# Add the markers to the map
+
+folium.Marker(location=least_used_location, popup=least_used_label, icon=folium.Icon(color="red")).add_to(chicago_map)
+
 chicago_map.save("chicago_map_bike_lanes.html")
